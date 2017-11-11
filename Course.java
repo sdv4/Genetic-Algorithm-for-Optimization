547608284064105811a3276e04d6467101a08601
shanesims <shane.sims.ss@gmail.com>
2017-11-11 13:46:55 -0700

    Course and Lab objects created

---
+++ Course.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Course {
+
+  private static final AtomicInteger counter = new AtomicInteger(0);
+
+  // Class attributes
+  private final String name;
+  private final int id;
+  private final int lectureNumber;
+
+  public Course(String name, int lectureNumber){
+    this.id = counter.incrementAndGet();
+    this.name = name;
+    this.lectureNumber = lectureNumber;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String name(){
+    return name;
+  }
+
+  public int getLecNum(){
+    return lectureNumber;
+  }
+  
+}
---
+++ Lab.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Lab {
+
+  private static final AtomicInteger counter = new AtomicInteger(0);
+
+  // Class attributes
+  private final String name;
+  private final int id;
+  private final int labNumber;
+  private final Course associatedLecture;                                       // TODO: possibly change this to: int LectureID
+
+  public Lab(String name, int labNumber, Course associatedLecture){
+    this.id = counter.incrementAndGet();
+    this.name = name;
+    this.labNumber = labNumber;
+    this.associatedLecture = associatedLecture;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String name(){
+    return name;
+  }
+
+  public int getLabNum(){
+    return labNumber;
+  }
+
+  public int getAssociatedLecId(){
+    return associatedLecture.getId();
+  }
+
+}
