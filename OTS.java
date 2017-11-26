49cda3eb17cd01523df0e41f45188562339e2f67
Justina Lem <CheezWhiz@Justinas-MacBook-Pro.local>
2017-11-25 18:54:04 -0700

    new

--- OTS.java
+++ OTS.java
@@ -19,6 +19,7 @@ public class OTS{
   protected ArrayList<Slot> slotCList;
   protected ArrayList<Slot> slotLList;
   protected int foundIndividual;
+  protected int[] root;
 
   protected Map<Integer,Integer> indexVector;                             // init as HashMap Key is the course/lab id and value is the index of the course/lab position in a solution vector
   //TODO: will need to discuss when to imitialize this.
@@ -34,6 +35,7 @@ public class OTS{
     this.courseLabList = coursesAndLabs;
     this.slotCList = courseSlots;
     this.slotLList = labSlots;
+    this.root = new int[courseLabList.size()];
   }
 
   //Nested class for Otree instantiation
@@ -44,11 +46,11 @@ public class OTS{
 
     protected otsNode parent;
     protected int depth;
-    protected ArrayList<Integer> assign;                                                     // let 0 represent ?, and 1-int.max represent time slot id
+    protected int[] assign;                                                     // let 0 represent ?, and 1-int.max represent time slot id
     protected int solvedStatus;
     protected ArrayList<otsNode> children;                                      //Note: using ArrayList to permit resizing with we delete 'no' nodes
 
-    otsNode(otsNode parent, ArrayList<Integer> assign){
+    otsNode(otsNode parent, int[] assign){
       this.parent = parent;
       this.assign = assign;
       this.solvedStatus = TBD;
@@ -71,7 +73,7 @@ public class OTS{
 		this.children = childrenArray;
 	}
 	
-	public ArrayList<Integer> getAssign(){
+	public int[] getAssign(){
 		return this.assign;
 	}
 	
@@ -90,13 +92,16 @@ public class OTS{
   }
   // end otsNode Class 
 
-	public ArrayList<Integer> getIndividual(){
+	/**
+	 * 
+	 */
+	public int[] getIndividual(){
 		foundIndividual = 0;
 		altern(root);
 		otsNode currentNode = root;
 		while (foundIndividual == 0){
 			currentNode = chooseNode(currentNode);
-			if (currentNode.getAssign().size() == courseLabList.size()){
+			if (isFullVector(currentNode){
 				foundIndividual = 1;
 			}
 			else{
@@ -108,45 +113,88 @@ public class OTS{
 		return currentNode.getAssign();
 	}
 	
-	private ArrayList<otsNode> altern(otsNode aNode){
-		ArrayList<Integer> parentVector = aNode.getAssign();
+	/**
+	 * Check if a vector has all classes assigned
+	 * @param the node with the vector to be checked
+	 * @return return true if all classes assigned, elsewise false.
+	 */ 
+	
+	public boolean isFullVector(otsNode aNode){
+		int[] aNodeVector = aNode.getAssign();
+		for (int i; i<aNode.length;i++){
+			if (aNodeVector[i] == 0)
+				return false;
+		}
+		return true;
+	}
+	/** 
+	 * Altern creates the branches for a given node depending on if the next class to be assigned
+	 * is a lecture or a lab
+	 * @param otsNode to branch from
+	 */
+	private void altern(otsNode aNode){
+		int[] parentVector = aNode.getAssign();				//get the vector from the node
 		ArrayList<otsNode> children = new ArrayList<>();
-		int size = parentVector.size();
-		if (courseLabList.get(size+1).isCourse()){
-			for (int i = 1; i<=slotCList.size(); i++){
-				ArrayList<Integer> copy = (ArrayList<Integer>) parentVector.clone();
-				copy.add(i);
-				children.add(new otsNode(aNode, copy));
+		int index = searchArray(parentVector, 0);			//get index of first unassigned class
+		if (index > -1){					
+			if (courseLabList.get(index).isCourse()){		//if index in vector is a course
+				for (int i = 1; i<=slotCList.size(); i++){	//create a branch for each course slot 
+					int[] copy = parentVector.clone();
+					copy[index] = i;
+					children.add(new otsNode(aNode, copy));
+					}
+				}
 			}
-		}
-		else{
-			for (int i = slotCList.size()+1; i<=slotCList.size()+slotLList.size(); i++){
-				ArrayList<Integer> copy = (ArrayList<Integer>) parentVector.clone();
-				copy.add(i);
-				children.add(new otsNode(aNode, copy));
+			else{											//if index in vector is a lab
+				for (int i = slotCList.size()+1; i<=slotCList.size()+slotLList.size(); i++){	//create a branch for each lab slot
+					int[] copy = parentVector.clone();
+					copy[index] = i;
+					children.add(new otsNode(aNode, copy));
+					}
+				}
 			}
+		}	
+	}
+	/**
+	 * Search for the first instance of x
+	 * @param an integer array and the key to find
+	 * @return returns -1 if key was not found, else the index of the key
+	 */ 
+	private int searchArray(int [] array, int x){	
+		int i= -1;
+		for (i = 0; i<array.length; i++){
+			if (int[i] == x)
+				break;
 		}
-		
+		return i;
 	}
 	
+	
+	/**
+	 * Choose a child node of aNode that has a valid vector.
+	 * If all children of aNode are invalid, recursively search through aNode's parent until a valid child node is found
+	 * @param the node whose children are being chosen
+	 * @return a valid child node
+	 */ 
 	private otsNode chooseNode(otsNode aNode){
 		ArrayList<otsNode> children = aNode.getChildren();
 		ArrayList<otsNode> validChildren = new ArrayList<>();
-		for (int i=0; i<children.size(); i++){
+		for (int i=0; i<children.size(); i++){					//Check if the children are valid vectors or not. 
+																//Add valid children to a separate list
 			if (children.get(i).getSolvedStatus() != 2 && constr(children.get(i).getAssign()) == true)
 			{
 				validChildren.add(children.get(i));
 			}
 			else{
-				children.get(i).setSolvedStatus(2);
+				children.get(i).setSolvedStatus(2);				//Set invalid children to solvedStatus = NO
 			}
 		}
 		
 		int randSize = validChildren.size();
 		
-		if (randSize == 0){
+		if (randSize == 0){				//if all children are invalid, go back to the aNode's parent node and choose a different node
 			aNode.setSolvedStatus(2);
-			//validChildren = chooseNode(aNode.getParent());
+			return chooseNode(aNode.getParent());
 		}
 		
 		randSize = validChildren.size();
